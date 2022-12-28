53e4ce8a1ab549a4c98870620f868a9da7d5bf9f
Andrei Siniavskii <siniavskij.andrei@yandex.ru>
2022-12-28 16:20:43 +0300

    state pattern

--- pattern/08_state.go
+++ pattern/08_state.go
@@ -1,9 +1,36 @@
 package pattern
 
+import "fmt"
+
 /*
 	Реализовать паттерн «состояние».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/State_pattern
 */
 
-// TODO
+type LightBulb struct {
+	isOn bool
+}
+
+func (lb *LightBulb) doStuffOn() {
+	fmt.Println("Emits light")
+}
+
+func (ln *LightBulb) doStuffOff() {
+	fmt.Println("Emits darkness")
+}
+
+func (lb *LightBulb) doStuff() {
+	if lb.isOn {
+		lb.doStuffOn()
+	} else {
+		lb.doStuffOff()
+	}
+}
+
+func example_state() {
+	lb := LightBulb{isOn: false}
+	lb.doStuff()
+	lb.isOn = true
+	lb.doStuff()
+}
